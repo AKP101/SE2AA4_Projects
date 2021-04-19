f1c53cb9c0684f140fe5f0e0a29c85f973b692f2
Anika Peer <47980558+AKP101@users.noreply.github.com>
2021-04-19 19:34:31 -0400

    Create AllTests.java
    Tests runner

---
+++ A3/src/AllTests.java
@@ -0,0 +1,22 @@
+/**
+ * Author: author
+ * Revised: date
+ * 
+ * Description: 
+ */
+
+package src;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   TestAttributeT.class,	
+   TestLOsT.class,
+   TestCourseT.class,
+   TestProgramT.class
+})
+
+public class AllTests{
+}
